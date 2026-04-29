0d76ae4ab297b429f3cb2d9460bf9d7b20960b17
Egor Pavlov <pavlovep@phystech.edu>
2026-04-29 15:49:24 +0000

    update for Tesla T4

--- natConvBox/postProcessing/meshSearsh.py
+++ natConvBox/postProcessing/meshSearsh.py
@@ -33,21 +33,31 @@ def processIter(N):
 if __name__ == "__main__":
     # Ns = [40, 80, 120, 160, 200]
     Ns = np.logspace(2, 3, 10, dtype=int)
-    Time = np.zeros(len(Ns))
-    itNums = np.zeros(len(Ns))
+
+    j = 3
+
+    log_step = (np.log10(3) - np.log10(2)) / (10 - 1)
+
+    log_min = np.log10(2)
+    log_max = np.log10(3) + j * log_step
+
+    Ns_extended = np.logspace(10**log_min, 10**log_max, 10 + j, dtype=int)
+
+    Time = np.zeros(len(Ns_extended))
+    itNums = np.zeros(len(Ns_extended))
 
     # with Pool(processes=4) as pool:  # По умолчанию использует все ядра CPU
     #     pool.map(processIter, Ns)
     
-    for ns in Ns:
+    for ns in Ns_extended:
         processIter(ns)
 
-    for i, N in enumerate(Ns):
+    for i, N in enumerate(Ns_extended):
         _, dt, itNum = np.loadtxt(f"case_{N}.txt")
         Time[i] = dt
         itNums[i] = itNum
 
-    np.savetxt("data.txt", np.array([Ns, Time, itNums]).T)
+    np.savetxt("data.txt", np.array([Ns_extended, Time, itNums]).T)
 
-    for i, N in enumerate(Ns):
+    for i, N in enumerate(Ns_extended):
         os.remove(f"case_{N}.txt")
